8890469354b76c3c20a03fba16c99d3092530053
MILLU44 <122621083+MILLU44@users.noreply.github.com>
2023-05-09 12:55:15 +0530

    Add files via upload

---
+++ temperature.js
@@ -0,0 +1,18 @@
+const inputFahrenheit = document.getElementById("inputFahrenheit");
+const inputCelsius = document.getElementById("inputCelsius");
+const inputKelvin = document.getElementById("inputKelvin");
+
+function fahrenheitConverter(value) {
+  inputCelsius.value = ((value - 32) / 1.8).toFixed(2); // ℃=(℉-32)/1.8
+  inputKelvin.value = ((value - 32) / 1.8 + 273.15).toFixed(2); // K=((℉-32)/1.8)+273.15
+}
+
+function celsiusConverter(value) {
+  inputFahrenheit.value = (value * 1.8 + 32).toFixed(2); // ℉=(℃*1.8)+32
+  inputKelvin.value = (Number(value) + 273.15).toFixed(2); // K=℃+273.15
+}
+
+function kelvinConverter(value) {
+  inputFahrenheit.value = ((value - 273.15) * 1.8 + 32).toFixed(2); // ℉=((K-273.15)*1.8)+32
+  inputCelsius.value = (value - 273.15).toFixed(2); // ℃=K-273.15
+}
